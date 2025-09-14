d8d1716b9dd2ba0e9f65ad2165336f679e27b5b3
luana-tacuatia <tacuatia@yahoo.com.br>
2025-09-14 20:45:17 -0300

    File app.py for testing.

---
+++ app.py
@@ -0,0 +1,25 @@
+import requests
+import pandas as pd
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+API_KEY = os.getenv('LASTAPI_KEY')
+
+st.title('Tendências Musicais com Last.fm API')
+
+artist = st.text_input('Digite o nome do artista:')
+
+if artist:
+    url = f'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist={artist}&api_key={API_KEY}&format=json'
+    response = requests.get(url)
+    data = response.json()
+
+    if "toptracks" in data:
+        tracks = data["toptracks"]["track"]
+        df = pd.DataFrame(tracks)
+        st.dataframe(df[['name', 'playcount', 'listeners']])
+    else:
+        st.error('Artista não encontrado ou erro na API.')
